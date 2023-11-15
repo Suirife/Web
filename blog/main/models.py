7faaca9d87a638b18b74355f05832aa515955ccc
Suirife <99396087+Suirife@users.noreply.github.com>
2023-11-15 20:54:26 +0700

    Add files via upload

--- blog/main/models.py
+++ blog/main/models.py
@@ -5,3 +5,12 @@ class article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+class tag(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+class comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+class category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
